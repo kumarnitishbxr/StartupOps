cf0f0e534565c69358bea63c91eb10cf0d89a051
Samir Singh <ersamirsingh@gmail.com>
2026-02-08 03:00:12 +0530

    Update: Update server acc to frontend auth

--- server/src/routes/AuthRoutes.js
+++ server/src/routes/AuthRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const authRouter = express.Router();
 
-import { Login, Logout, Register } from '../Controllers/authController.js';
+import { Login, Logout, Register, checkUser } from '../Controllers/authController.js';
 import optionalMiddleware from '../middleware/optionalMiddleware.js';
 
 authRouter.post('/login', Login);
 authRouter.post('/register', Register);
 authRouter.get('/logout', optionalMiddleware, Logout);
+authRouter.get('/check', optionalMiddleware, checkUser);
 
 export default authRouter;
