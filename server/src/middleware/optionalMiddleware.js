9fcfd28f43285dff0493dae736b623b90db4ebea
Samir Singh <ersamirsingh@gmail.com>
2026-02-07 21:55:50 +0530

    Startup router and controller done

---
+++ server/src/middleware/optionalMiddleware.js
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import redisClient from "../config/redis.js";
+
+
+const optionalMiddleware = async (req, res, next) => {
+
+  try {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    // console.log(token)
+    if (!token) {
+      return res.status(401).json({ message: "No token, authorization denied" });
+    }
+
+    const payload = jwt.verify(token, process.env.SECRET_KEY);
+    // console.log(payload)
+    if (!payload)
+      return res.status(404).json({
+        success: false,
+        message: "Invalid token"
+      })
+
+    const isBlocked = await redisClient.exists(`token ${token}`)
+    if (isBlocked)
+      return res.status(400).json({
+        message: 'Invalid Token'
+      })
+
+    const user = await User.findById(payload.id).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export default optionalMiddleware;
---
+++ server/src/routes/startupRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { addStartup, getAllStartups, getStartupById, updateStartupById, deleteStartup } from '../Controllers/startupController.js';
+import founderMiddleware from '../middleware/FounderMiddleware.js';
+import optionalMiddleware from '../middleware/optionalMiddleware.js';
+
+const startupRouter = express.Router();
+
+
+startupRouter.post('/', founderMiddleware, addStartup);
+startupRouter.get('/', optionalMiddleware, getAllStartups);
+startupRouter.get('/:startupId', optionalMiddleware, getStartupById);
+startupRouter.patch('/:startupId', founderMiddleware, updateStartupById);
+startupRouter.delete('/:startupId', founderMiddleware, deleteStartup);
+
+
+export default startupRouter;
